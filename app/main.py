8e00ba07bb14465a5ca4a6a01a997fccac5b8cf9
Debjyoti Paul <11320427+DebjyotiPaul@users.noreply.github.com>
2024-10-06 17:28:42 +0530

    decode bencoded dict

--- app/main.py
+++ app/main.py
@@ -20,8 +20,8 @@ def decode_bencode(bencoded_value):
     elif (chr(bencoded_value[0]) == "l" or chr(bencoded_value[0]) == "d") and chr(bencoded_value[-1]) == "e":
         return bencodepy.decode(bencoded_value)
     
-    '''elif chr(bencoded_value[0]) == "d" and chr(bencoded_value[-1]) == "e":
-        return bencodepy.decode(bencoded_value)'''    
+    #elif chr(bencoded_value[0]) == "d" and chr(bencoded_value[-1]) == "e":
+        #return bencodepy.decode(bencoded_value)    
     else:
        raise NotImplementedError("Only strings are supported at the moment")
        
